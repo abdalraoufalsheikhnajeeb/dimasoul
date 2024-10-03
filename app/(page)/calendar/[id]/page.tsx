be6512621689232784f662c31992056bd5daec24
MHD Firass Barakat <firass.barakat1998@gmail.com>
2024-10-03 06:54:29 +0300

    working on design notes

--- app/(page)/calendar/[id]/page.tsx
+++ app/(page)/calendar/[id]/page.tsx
@@ -1,20 +1,17 @@
-"use client";
-
-import ProgramCalendar from "@/app/components/ProgramCalendar";
+import CalenderEventCard from "@/app/components/CalenderEventCard";
+import React from "react";
 
 const page = () => {
   return (
-    <div>
+    <>
       <section className="bg-primaryBG">
-        <div className="container max-w-[90vw] px-4 py-8 text-center md:py-16 md:text-left lg:max-w-7xl">
-          {/* Services Section */}
+        <div className="mx-auto max-w-[90vw] px-4 py-8 text-center md:py-16 md:text-left lg:max-w-7xl">
           <div className="mt-16 md:flex md:items-center md:space-x-8">
-            {/* Vertical Line */}
             <div className="hidden bg-primary md:block md:h-full md:w-1"></div>
-
-            {/* Services Text */}
-            <div className="border-l-4 border-primary pl-6 md:w-1/2">
-              <h2 className="mb-4 text-4xl font-bold text-primary">Calender</h2>
+            <div className="container border-l-4 border-primary pl-6 md:w-1/2">
+              <h2 className="mb-4 text-4xl font-bold text-primary">
+                Calender Day Details
+              </h2>
               <p className="text-lg leading-relaxed text-gray-600">
                 Lorem ipsum dolor sit amet consectetur. Convallis est urna
                 adipiscing fringilla nulla diam lorem non mauris. Ultrices
@@ -25,8 +22,16 @@ const page = () => {
           </div>
         </div>
       </section>
-      <ProgramCalendar />
-    </div>
+      <section className="container flex flex-col gap-10 py-10">
+        <CalenderEventCard />
+        <CalenderEventCard />
+        <CalenderEventCard />
+        <CalenderEventCard />
+        <CalenderEventCard />
+        <CalenderEventCard />
+      </section>
+    </>
   );
 };
+
 export default page;
